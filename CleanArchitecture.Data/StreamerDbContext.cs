34abf9f81dc275a2a43026f76d8b6c8e863c4abc
Jose Ayala <joseayala45@outlook.es>
2024-08-29 18:15:47 -0500

    feature: add context, create tables and successfully connection with database.

---
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        //manejar conexion con base de datos
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@"Data Source=localhost; Initial Catalog=Streamer;Integrated Security=False");
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-S18FHFS;Database=Streamer;Integrated Security=True;TrustServerCertificate=True");
+        }
+        public DbSet<Streamer>? Streamers {  get; set; } 
+        public DbSet<Video>? Videos { get; set; }
+    }
+}
