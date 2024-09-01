094b140a34d1419b264c9fc1446633edc1a7e27b
Jose Ayala <joseayala45@outlook.es>
2024-09-01 10:14:47 -0500

    feature: implement Logger on Console

--- CleanArchitecture.ConsoleApp/Program.cs
+++ CleanArchitecture.ConsoleApp/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using CleanArchitecture.Data;
+using CleanArchitecture.Domain;
+
+//instancia de dbContext
+StreamerDbContext dbContext = new();
+//objeto directo de dbcontext -> permitira insertar en la tabla Streamer
+Streamer streamer = new Streamer()
+{
+    Name = "Amazon Prime",
+    Url = "https://www.amazonprime.com"
+};
+
+//es signo de admiracion me permite informar que el objeto existe
+dbContext!.Streamers!.Add(streamer);
+//para guardar la insercion
+await dbContext.SaveChangesAsync();
+
+var movies = new List<Video>
+{
+    new Video
+    {
+        Name = "Mad Max",
+        StreamerId = 1
+    },
+    new Video
+    {
+        Name = "Accidente",
+        StreamerId = streamer.Id
+    },
+        new Video
+    {
+        Name = "Crepusculo",
+        StreamerId = streamer.Id
+    },
+            new Video
+    {
+        Name = "Citizen Kane",
+        StreamerId = streamer.Id
+    }
+};
+
+//para guardar un cambio en una lista un arreglo
+await dbContext.AddRangeAsync(movies);
+//para guardar en el contexto
+await dbContext.SaveChangesAsync();
--- CleanArchitecture.Data/StreamerDbContext.cs
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -9,7 +9,11 @@ namespace CleanArchitecture.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //optionsBuilder.UseSqlServer(@"Data Source=localhost; Initial Catalog=Streamer;Integrated Security=False");
-            optionsBuilder.UseSqlServer(@"Server=DESKTOP-S18FHFS;Database=Streamer;Integrated Security=True;TrustServerCertificate=True");
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-S18FHFS;Database=Streamer;Integrated Security=True;TrustServerCertificate=True")
+                //Para imprimir todas las consultas por consola
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, Microsoft.Extensions.Logging.LogLevel.Information)
+                //permite describir cada una de las operaciones
+                .EnableSensitiveDataLogging();
         }
         public DbSet<Streamer>? Streamers {  get; set; } 
         public DbSet<Video>? Videos { get; set; }
