ba6c4542f00e47bd98229a04581cfd64c0df1205
Jose Ayala <joseayala45@outlook.es>
2024-09-02 14:14:07 -0500

    refactor: refactor architecture, implement cleanArchitectur.

--- CleanArchitecture.Domain/Entities/VideoActor.cs
+++ CleanArchitecture.Domain/Entities/VideoActor.cs
@@ -1,6 +1,8 @@
-﻿namespace CleanArchitecture.Domain.Entities
+﻿using CleanArchitecture.Domain.Common;
+
+namespace CleanArchitecture.Domain.Entities
 {
-    public class VideoActor
+    public class VideoActor : BaseDomainModel
     {
         public int VideoId { get; set; }
         public int ActorId { get; set; }
