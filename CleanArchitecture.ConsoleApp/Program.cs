c4859cf2909ead3c2981e086663f40b4650a9b11
Jose Ayala <joseayala45@outlook.es>
2024-09-02 10:36:25 -0500

    feature: get multiple relations.

--- CleanArchitecture.ConsoleApp/Program.cs
+++ CleanArchitecture.ConsoleApp/Program.cs
@@ -19,10 +19,36 @@ StreamerDbContext dbContext = new();
 //await AddNewStreamerWithVideoId();
 //await AddNewActorWithVideo();
 //await AddNewDirectorWithVideo();
+await MultipleEntitesQuery();
 
 Console.WriteLine("presione cualquier tecla para terminar el programa");
 //Para cerrar la consola con cualquier tecla
 Console.ReadKey();
+async Task MultipleEntitesQuery()
+{
+    //sentencia include con lambda para incluir las tablas
+    //var videoWithActores = await dbContext!.Videos!.Include(q => q.Actors).FirstOrDefaultAsync(q => q.Id == 1);
+
+    //var actor = await dbContext!.Actors!.Select(q => q.Name).ToListAsync();
+
+    var videoWithDirector = await dbContext!.Videos!
+                            .Where(q => q.Director != null)
+                            .Include(q => q.Director)
+                            .Select(q =>
+                               new
+                               {
+                                   Director_Nombre_Completo = $"{q.Director.Name} {q.Director.LastName}",
+                                   Movie = q.Name
+                               }
+                             )
+                            .ToListAsync();
+
+
+    foreach (var pelicula in videoWithDirector)
+    {
+        Console.WriteLine($"{pelicula.Movie}  - {pelicula.Director_Nombre_Completo} ");
+    }
+}
 async Task AddNewDirectorWithVideo()
 {
     var director = new Director
