598e603c0717d73d136c7fb27490222f2cd8f8a2
Jose Ayala <joseayala45@outlook.es>
2024-09-04 18:33:03 -0500

    feature: dependecy injection in layer infrastructure.

---
+++ CleanArchitecture.Data/InfrastructureServiceRegistration.cs
@@ -0,0 +1,31 @@
+﻿using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.Models;
+using CleanArchitecture.Infrastructure.Persistence;
+using CleanArchitecture.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            //creacion de la conexion a la base de datos, leyendo el connectionString ubicado en API
+            services.AddDbContext<StreamerDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("connectionString"))
+            );
+            //ingresando las inyeccioness
+            //inyeccion de repositorybase
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            //inyeccion de repositories
+            services.AddScoped<IVideoRepository, VideoRepository>();
+            services.AddScoped<IStreamerRepository, StreamerRepository>();
+
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+
+            return services;
+        }
+    }
+}
