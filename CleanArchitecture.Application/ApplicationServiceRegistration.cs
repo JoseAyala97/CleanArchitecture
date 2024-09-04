4cf1e8132c967260a7c37a03ccb7286d39794f7f
Jose Ayala <joseayala45@outlook.es>
2024-09-04 16:45:00 -0500

    feature: injection dependecy.

---
+++ CleanArchitecture.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchitecture.Application.Behaviours;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CleanArchitecture.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            //registro: lo que haran sera buscar donde se usen estas dependencias y realizara la injection
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            //trasient
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnHandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>),typeof(ValidationBehaviour<,>));
+
+
+            return services;
+        }
+    }
+}
